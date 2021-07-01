79718afb3d16d95864141e18965130b7611e2aa4
mishustin-git <mishustin.post@yandex.ru>
2021-07-01 10:05:15 +0300

    first page all screen without

--- first-three-pages/js/legenda-original.js
+++ first-three-pages/js/legenda-original.js
@@ -1,2 +1,4 @@
 var scene = document.getElementById('parallax-img-original-hd');
 var parallaxInstance = new Parallax(scene);
+var scene1 = document.getElementById('parallax-img-original-b3');
+var parallaxInstance = new Parallax(scene1);
