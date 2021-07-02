11f64b74c71b5a5828f7f3199b46f61c1c57b6ad
mishustin-git <mishustin.post@yandex.ru>
2021-07-02 14:41:07 +0300

    first screen adaptive

--- first-three-pages/js/legenda-original.js
+++ first-three-pages/js/legenda-original.js
@@ -2,3 +2,5 @@ var scene = document.getElementById('parallax-img-original-hd');
 var parallaxInstance = new Parallax(scene);
 var scene1 = document.getElementById('parallax-img-original-b3');
 var parallaxInstance = new Parallax(scene1);
+var scene1 = document.getElementById('parallax-img-original-b3');
+var parallaxInstance = new Parallax(scene1);
